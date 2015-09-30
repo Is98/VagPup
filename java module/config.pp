f506f75a4a4a5d1bf82e51b138dec3dc16c908f1
alanhughes <alanhughes1992@gmail.com>
2015-09-30 11:01:12 +0100

    correction

--- java module/config.pp
+++ java module/config.pp
@@ -1,9 +1,9 @@
 class java::config {
-	$javaVersion = jdk1.8.0_45
+	$jmavenVersion = jdk1.8.0_45
 	
 	file { '/etc/profile.d/java.sh':
 		ensure => present
-		content => "export JAVA_HOME=/opt/'${javaVersion}'\n
+		content => "export M2_HOME=/opt/'${mavenVersion}'\n
 export JRE_HOME=/opt/'${javaVersion}'\n
 export PATH =\$PATH:/opt/'${javaVersion}'/bin:/opt/'${javaVersion}'/jre/bin/"
 	}
--- java module/install.pp
+++ java module/install.pp
@@ -1,27 +1,27 @@
 class java::install {
-	$distServer = http://10.50.20.49:8080/aaron
-	$javaTarball = jdk-8u45-linux-x64.tar.gz
-	$javaVersion = jdk1.8.0_45
-	$installLocation = /usr/bin/java
+	$distServer = "http://10.50.20.49:8080/aaron",
+	$javaTarball = "jdk-8u45-linux-x64.tar.gz",
+	$javaVersion = "jdk1.8.0_45",
+	$installLocation = "/usr/bin/java",
 	
 	exec { 'retrieve file':
-		cwd => "/opt/"
-		command => "wget '${distServer}'/downloads/'${javaTarball}'"
+		cwd => "/opt/",
+		command => "wget '${distServer}'/downloads/'${javaTarball}'",
 	}
 	exec { 'unzip file':
-		cwd => "/opt/"
-		command => "tar zxvf '${javaTarball}'"
-		require => Exec['retrieve file']
+		cwd => "/opt/",
+		command => "tar zxvf '${javaTarball}'",
+		require => Exec['retrieve file'],
 	}
 	exec { 'install java':
-		cwd => "/opt/'${javaVersion}'"
-		command => "update-alternatives --install '${installLocation}' java /opt/'${javaVersion}'/bin/java 2"
-		require => Exec['unzip file']
+		cwd => "/opt/'${javaVersion}'",
+		command => "update-alternatives --install '${installLocation}' java /opt/'${javaVersion}'/bin/java 2",
+		require => Exec['unzip file'],
 	}
 	exec { 'set java':
-		cwd => "/opt/'${javaVersion}'"
-		command => "update-alternatives --config java -y"
-		require => Exec['install java']
+		cwd => "/opt/'${javaVersion}'",
+		command => "update-alternatives --config java -y",
+		require => Exec['install java'],
 	}
 
 }
